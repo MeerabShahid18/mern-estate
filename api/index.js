c0502f4fb2d3da260bc4b9c0cfb226cd072429c8
MeerabShahid18 <meerabshahid866@gmail.com>
2026-02-20 14:59:43 +0500

    create a test api router

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express=require("express")
 const app=express();
 const mongoose=require("mongoose");
+const userRouter=require("./routers/user.router")
 mongoose.connect("mongodb://127.0.0.1:27017/mern-estate")
 .then(()=> console.log("Connected to MongoDb"))
 .catch((err)=> console.log(err));
-
+app.use(express.json());
+app.use("/api/user", userRouter);
 app.listen(3000, ()=> console.log("server is running on port 3000"));
