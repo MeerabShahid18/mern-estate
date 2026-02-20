c422bea406acb2e33badaddaa15a1bd969609a37
MeerabShahid18 <meerabshahid866@gmail.com>
2026-02-20 10:41:23 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const express=require("express")
 const app=express();
-
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/mern-estate")
+.then(()=> console.log("Connected to MongoDb"))
+.catch((err)=> console.log(err));
 
 app.listen(3000, ()=> console.log("server is running on port 3000"));
