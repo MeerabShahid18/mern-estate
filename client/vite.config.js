ed622a4312da9588c4c9e26a4f951ba0805c8f1b
MeerabShahid18 <meerabshahid866@gmail.com>
2026-02-22 23:26:28 +0500

    Complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }
+
+    }
+  },
   plugins: [
     react(),
     tailwindcss(),
