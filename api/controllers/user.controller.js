920a53a6b40048d5c01be158aebea51c90dced43
MeerabShahid18 <meerabshahid866@gmail.com>
2026-03-20 16:15:12 +0500

    Add contact landlord functionality to the listing page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -53,8 +53,24 @@ const getUserListings=async(req, res, next)=>{
          return next(errorHnadler(401, 'You can only view your listing'));
     }
 }
+
+const getUser = async (req, res, next) => {
+  try {
+    
+    const user = await User.findById(req.params.id);
+  
+    if (!user) return next(errorHandler(404, 'User not found!'));
+  
+    const { password: pass, ...rest } = user._doc;
+  
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports={
     updateUser,
     deleteUser,
-    getUserListings
+    getUserListings,
+    getUser
 }
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useEffect } from 'react'
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
 import {Swiper, SwiperSlide} from 'swiper/react'
 import SwiperCore from 'swiper'
@@ -14,13 +15,16 @@ import {
   FaShare,
 } from 'react-icons/fa';
 import 'swiper/css/bundle'
+import Contact from '../components/Contact';
 export default function Listing() {
     SwiperCore.use([Navigation]);
     const [listing, setListing]=useState(null);
     const [loading, setLoading]=useState(false);
     const [error, setError]=useState(false);
     const [copied, setCopied] = useState(false);
+     const [contact, setContact] = useState(false); 
     const params=useParams();
+    const {currentUser}=useSelector((state)=> state.user);
     useEffect(()=>{ 
         const fetchListing=async()=>{
             try {
@@ -123,6 +127,12 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-90 p-3'>
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing}/>}
           </div>
             </div>
         )}
